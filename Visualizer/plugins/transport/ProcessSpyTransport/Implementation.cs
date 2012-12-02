4b4fe45a82c8e5578e13e885181a23bec7d319b5
David Kahler <dakahler@gmail.com>
2012-12-01 21:39:26 -0800

    Added ProcessSpy transport plugin. Full implementation to come.

---
+++ Visualizer/plugins/transport/ProcessSpyTransport/Implementation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Alloclave_Plugin
+{
+	[Export(typeof(Alloclave.Transport))]
+	[ExportMetadata("Name", "Process Spy")]
+	public class ProcessSpyTransport : Alloclave.Transport
+	{
+		static Alloclave.TargetSystemInfo temp;
+		Process TargetProcess;
+
+		public ProcessSpyTransport()
+			: base(temp)
+		{
+			
+		}
+
+		public ProcessSpyTransport(Alloclave.TargetSystemInfo targetSystemInfo)
+			: base(targetSystemInfo)
+		{
+			
+		}
+
+		public override void Connect()
+		{
+
+		}
+
+		public override void Disconnect()
+		{
+
+		}
+
+		public override void SpawnCustomUI()
+		{
+			ProcessForm processForm = new ProcessForm();
+			if (processForm.ShowDialog() == DialogResult.OK)
+			{
+				String targetProcessName = processForm.ProcessComboBox.SelectedItem.ToString();
+				Process[] processes = Process.GetProcesses();
+				foreach (Process process in processes)
+				{
+					if (targetProcessName == process.ProcessName)
+					{
+						TargetProcess = process;
+						break;
+					}
+				}
+			}
+		}
+	}
+}
