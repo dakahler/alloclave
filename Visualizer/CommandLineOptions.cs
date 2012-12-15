c43217797df7ba65b5206a1f9a67213028f4a8b8
David Kahler <dakahler@gmail.com>
2012-12-15 10:24:09 -0800

    Added missing CommandLineOptions class.

---
+++ Visualizer/CommandLineOptions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace Alloclave
+{
+	public class CommandLineOptions
+	{
+		[Option("t", "transport", Required = false, HelpText = "Transport type to select.")]
+		public string TransportType { get; set; }
+
+		[Option("p", "process", Required = false, HelpText = "Process name for process spy transport.")]
+		public string ProcessName { get; set; }
+
+		[HelpOption]
+		public string GetUsage()
+		{
+			var usage = new StringBuilder();
+			usage.AppendLine("Alloclave Visualizer");
+			usage.AppendLine("Temp usage instructions...");
+			return usage.ToString();
+		}
+	}
+}
