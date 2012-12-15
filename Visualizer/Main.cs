e8e78eeedcf0e98b88eefd625bc56d7e5cd18536
David Kahler <dakahler@gmail.com>
2012-12-15 14:09:18 -0800

    Capture mouse scroll wheel events and forward them onto the control the mouse is hovering over. Fixes the issue of scroll zooming not working when the address space control doesn't have focus.

--- Visualizer/Main.cs
+++ Visualizer/Main.cs
@@ -8,16 +8,26 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using CommandLine;
+using System.Runtime.InteropServices;
 
 namespace Alloclave
 {
-	public partial class Main : Form
+	public partial class Main : Form, IMessageFilter
 	{
+		[DllImport("user32.dll")]
+		private static extern IntPtr WindowFromPoint(Point pt);
+		[DllImport("user32.dll")]
+		private static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wp, IntPtr lp);
+
+		const uint WM_MOUSEWHEEL = 0x20a;
+
 		CommandLineOptions options = new CommandLineOptions();
 
 		public Main()
 		{
 			InitializeComponent();
+
+			Application.AddMessageFilter(this);
 
 			ICommandLineParser parser = new CommandLineParser();
 			String[] args = Environment.GetCommandLineArgs();
@@ -37,6 +47,22 @@ namespace Alloclave
 			}
 
 			licenseToolStripMenuItem.Text = Licensing.LicenseName;
+		}
+
+		public bool PreFilterMessage(ref Message m)
+		{
+			if (m.Msg == WM_MOUSEWHEEL)
+			{
+				// WM_MOUSEWHEEL, find the control at screen position m.LParam
+				Point pos = new Point(m.LParam.ToInt32() & 0xffff, m.LParam.ToInt32() >> 16);
+				IntPtr hWnd = WindowFromPoint(pos);
+				if (hWnd != IntPtr.Zero && hWnd != m.HWnd && Control.FromHandle(hWnd) != null)
+				{
+					SendMessage(hWnd, m.Msg, m.WParam, m.LParam);
+					return true;
+				}
+			}
+			return false;
 		}
 
 		private void newToolStripMenuItem_Click(object sender, EventArgs e)
