7bd40e7a1f5674771678bd929e1812660c3fc13f
David Kahler <dakahler@gmail.com>
2012-12-23 18:27:10 -0800

    Comparison implementations.

--- Visualizer/plugins/transport/ProcessSpyTransport/HeapWalker/HeapWalker.h
+++ Visualizer/plugins/transport/ProcessSpyTransport/HeapWalker/HeapWalker.h
@@ -4,11 +4,45 @@
 
 namespace Alloclave_Plugin {
 
-	public ref class AllocationData
+	public ref class AllocationData : public System::IComparable
 	{
 	public:
 		System::UInt64 Address;
 		System::UInt64 Size;
+
+		virtual int CompareTo(System::Object^ other)
+		{
+			AllocationData^ allocationData = (AllocationData^)other;
+
+			if (Address < allocationData->Address)
+			{
+				return -1;
+			}
+			else if (Address > allocationData->Address)
+			{
+				return 1;
+			}
+			else
+			{
+				return 0;
+			}
+		}
+	};
+
+	public ref class AllocationDataEqualityComparer : public System::Collections::Generic::IEqualityComparer<AllocationData^>
+	{
+	public:
+		virtual bool Equals(AllocationData^ first, AllocationData^ second)
+		{
+			return first->Address == second->Address && first->Size == second->Size;
+		}
+
+		virtual int GetHashCode(AllocationData^ allocationData)
+		{
+			int hCode = (int)(allocationData->Address * allocationData->Size);
+			return hCode.GetHashCode();
+		}
+
 	};
 
 	public ref class HeapWalker
