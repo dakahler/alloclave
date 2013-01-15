070c78efe727fd6cd66dfc8c3bfb9fad5bdd900d
David Kahler <dakahler@gmail.com>
2013-01-14 23:20:15 -0800

    Added MemoryBlockManager, future container of VisualMemoryBlocks.

---
+++ Visualizer/MemoryBlockManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alloclave
+{
+	// TODO: Implement
+	class MemoryBlockManager
+	{
+		private SortedList<UInt64, VisualMemoryBlock> VisualMemoryBlocks = new SortedList<UInt64, VisualMemoryBlock>();
+	}
+}
