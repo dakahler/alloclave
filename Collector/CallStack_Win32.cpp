0ecc9a2484d889a992989c51434a74cf892c1061
David Kahler <dakahler@gmail.com>
2013-04-10 18:36:56 -0700

    Minor style changes.

--- Collector/CallStack_Win32.cpp
+++ Collector/CallStack_Win32.cpp
@@ -17,33 +17,33 @@ void CallStack_Win32::Rebuild()
 {
 	CallStack::Rebuild();
 
-	::CONTEXT lContext;
-	::ZeroMemory( &lContext, sizeof( ::CONTEXT ) );
-	::RtlCaptureContext( &lContext );
+	CONTEXT lContext;
+	ZeroMemory(&lContext, sizeof(CONTEXT));
+	RtlCaptureContext(&lContext);
 
-	::STACKFRAME64 lFrameStack;
-	::ZeroMemory( &lFrameStack, sizeof( ::STACKFRAME64 ) );
+	STACKFRAME64 lFrameStack;
+	ZeroMemory(&lFrameStack, sizeof(STACKFRAME64));
 	lFrameStack.AddrPC.Offset = lContext.Eip;
 	lFrameStack.AddrFrame.Offset = lContext.Ebp;
 	lFrameStack.AddrStack.Offset = lContext.Esp;
 	lFrameStack.AddrPC.Mode = lFrameStack.AddrFrame.Mode = lFrameStack.AddrStack.Mode = AddrModeFlat;
 
 	// TODO: x64
-	::DWORD lTypeMachine = IMAGE_FILE_MACHINE_I386;
+	DWORD lTypeMachine = IMAGE_FILE_MACHINE_I386;
 
 	int i;
-	for (i = ::DWORD(); i < MaxStackDepth; i++)
+	for (i = DWORD(); i < MaxStackDepth; i++)
 	{
-		if( !::StackWalk64( lTypeMachine, ::GetCurrentProcess(), ::GetCurrentThread(), &lFrameStack, lTypeMachine == IMAGE_FILE_MACHINE_I386 ? 0 : &lContext,
-			nullptr, &::SymFunctionTableAccess64, &::SymGetModuleBase64, nullptr ) )
+		if( !StackWalk64( lTypeMachine, GetCurrentProcess(), GetCurrentThread(), &lFrameStack, lTypeMachine == IMAGE_FILE_MACHINE_I386 ? 0 : &lContext,
+			nullptr, &SymFunctionTableAccess64, &SymGetModuleBase64, nullptr ) )
 		{
 			break;
 		}
 		if (lFrameStack.AddrPC.Offset != 0)
 		{
-			::MEMORY_BASIC_INFORMATION lInfoMemory;
-			::VirtualQuery( ( ::PVOID )lFrameStack.AddrPC.Offset, &lInfoMemory, sizeof( lInfoMemory ) );
-			::DWORD64 lBaseAllocation = reinterpret_cast< ::DWORD64 >( lInfoMemory.AllocationBase );
+			MEMORY_BASIC_INFORMATION lInfoMemory;
+			VirtualQuery((PVOID)lFrameStack.AddrPC.Offset, &lInfoMemory, sizeof(lInfoMemory));
+			DWORD64 lBaseAllocation = reinterpret_cast<DWORD64>(lInfoMemory.AllocationBase);
 
 			DWORD64 finalAddress = lFrameStack.AddrPC.Offset - lBaseAllocation;
 			Push((void*)finalAddress);
