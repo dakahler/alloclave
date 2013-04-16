9d8b3030d0b7119166d9ded514a8f3a967e0931c
David Kahler <dakahler@gmail.com>
2013-04-15 19:27:19 -0700

    Added Alloclave.h, which provides a single location for user modification of SDK behavior.

---
+++ Collector/Alloclave.h
@@ -0,0 +1,21 @@
+#ifndef _ALLOCLAVE_H
+#define _ALLOCLAVE_H
+
+// Set this to 0 to disable Alloclave completely
+#define ALLOCLAVE_ENABLED				1
+
+// Set this to 0 if you have your own new/delete overrides
+#define ALLOCLAVE_OVERRIDE_NEWDELETE	1
+
+// Set this to 0 if you have your own malloc/free overrides,
+// or do not want to override malloc/free
+#define ALLOCLAVE_OVERRIDE_MALLOC		1
+
+// NOTE: If you set the transport type to "Custom",
+// you must register your transport instance via
+// Alloclave::RegisterTransport
+#define TRANSPORT_TYPE_WIN32			0
+#define TRANSPORT_TYPE_CUSTOM			1
+#define ALLOCLAVE_TRANSPORT_TYPE		TRANSPORT_TYPE_WIN32
+
+#endif // _ALLOCLAVE_H
