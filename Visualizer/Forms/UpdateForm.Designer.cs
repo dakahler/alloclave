5b63870e7d5b4ce6711b4df65737661167fd812d
David Kahler <dakahler@gmail.com>
2013-04-16 21:28:57 -0700

    Minor sizing/control box tweaks to modal forms.

--- Visualizer/Forms/UpdateForm.Designer.cs
+++ Visualizer/Forms/UpdateForm.Designer.cs
@@ -28,131 +28,142 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.label1 = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
-			this.label3 = new System.Windows.Forms.Label();
-			this.CurrentVersionLabel = new System.Windows.Forms.Label();
-			this.NewVersionLabel = new System.Windows.Forms.Label();
-			this.label4 = new System.Windows.Forms.Label();
-			this.ChangesTextBox = new System.Windows.Forms.TextBox();
-			this.OkButton = new System.Windows.Forms.Button();
-			this._CancelButton = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(37, 26);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(217, 22);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "An update is available!";
-			// 
-			// label2
-			// 
-			this.label2.AutoSize = true;
-			this.label2.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label2.Location = new System.Drawing.Point(48, 61);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(111, 16);
-			this.label2.TabIndex = 1;
-			this.label2.Text = "Current Version:";
-			// 
-			// label3
-			// 
-			this.label3.AutoSize = true;
-			this.label3.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label3.Location = new System.Drawing.Point(48, 83);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(89, 16);
-			this.label3.TabIndex = 2;
-			this.label3.Text = "New Version:";
-			// 
-			// CurrentVersionLabel
-			// 
-			this.CurrentVersionLabel.AutoSize = true;
-			this.CurrentVersionLabel.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.CurrentVersionLabel.Location = new System.Drawing.Point(165, 61);
-			this.CurrentVersionLabel.Name = "CurrentVersionLabel";
-			this.CurrentVersionLabel.Size = new System.Drawing.Size(48, 16);
-			this.CurrentVersionLabel.TabIndex = 3;
-			this.CurrentVersionLabel.Text = "1.0.0.0";
-			// 
-			// NewVersionLabel
-			// 
-			this.NewVersionLabel.AutoSize = true;
-			this.NewVersionLabel.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.NewVersionLabel.Location = new System.Drawing.Point(165, 83);
-			this.NewVersionLabel.Name = "NewVersionLabel";
-			this.NewVersionLabel.Size = new System.Drawing.Size(52, 16);
-			this.NewVersionLabel.TabIndex = 4;
-			this.NewVersionLabel.Text = "1.1.0.0";
-			// 
-			// label4
-			// 
-			this.label4.AutoSize = true;
-			this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label4.Location = new System.Drawing.Point(48, 122);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(75, 18);
-			this.label4.TabIndex = 5;
-			this.label4.Text = "Changes:";
-			// 
-			// ChangesTextBox
-			// 
-			this.ChangesTextBox.BackColor = System.Drawing.SystemColors.Window;
-			this.ChangesTextBox.Location = new System.Drawing.Point(41, 143);
-			this.ChangesTextBox.Multiline = true;
-			this.ChangesTextBox.Name = "ChangesTextBox";
-			this.ChangesTextBox.ReadOnly = true;
-			this.ChangesTextBox.Size = new System.Drawing.Size(380, 178);
-			this.ChangesTextBox.TabIndex = 6;
-			// 
-			// OkButton
-			// 
-			this.OkButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.OkButton.Location = new System.Drawing.Point(217, 336);
-			this.OkButton.Name = "OkButton";
-			this.OkButton.Size = new System.Drawing.Size(98, 23);
-			this.OkButton.TabIndex = 7;
-			this.OkButton.Text = "Update Now";
-			this.OkButton.UseVisualStyleBackColor = true;
-			this.OkButton.Click += new System.EventHandler(this.OkButton_Click);
-			// 
-			// CancelButton
-			// 
-			this._CancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this._CancelButton.Location = new System.Drawing.Point(331, 336);
-			this._CancelButton.Name = "CancelButton";
-			this._CancelButton.Size = new System.Drawing.Size(75, 23);
-			this._CancelButton.TabIndex = 8;
-			this._CancelButton.Text = "Cancel";
-			this._CancelButton.UseVisualStyleBackColor = true;
-			this._CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
-			// 
-			// UpdateForm
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.CancelButton = this._CancelButton;
-			this.ClientSize = new System.Drawing.Size(454, 371);
-			this.Controls.Add(this._CancelButton);
-			this.Controls.Add(this.OkButton);
-			this.Controls.Add(this.ChangesTextBox);
-			this.Controls.Add(this.label4);
-			this.Controls.Add(this.NewVersionLabel);
-			this.Controls.Add(this.CurrentVersionLabel);
-			this.Controls.Add(this.label3);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.label1);
-			this.MaximizeBox = false;
-			this.MinimizeBox = false;
-			this.Name = "UpdateForm";
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-			this.Text = "Update Alloclave";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CurrentVersionLabel = new System.Windows.Forms.Label();
+            this.NewVersionLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ChangesTextBox = new System.Windows.Forms.TextBox();
+            this.OkButton = new System.Windows.Forms.Button();
+            this._CancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(273, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "An update is available!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(64, 75);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Current Version:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(64, 102);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "New Version:";
+            // 
+            // CurrentVersionLabel
+            // 
+            this.CurrentVersionLabel.AutoSize = true;
+            this.CurrentVersionLabel.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CurrentVersionLabel.Location = new System.Drawing.Point(220, 75);
+            this.CurrentVersionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.CurrentVersionLabel.Name = "CurrentVersionLabel";
+            this.CurrentVersionLabel.Size = new System.Drawing.Size(60, 19);
+            this.CurrentVersionLabel.TabIndex = 3;
+            this.CurrentVersionLabel.Text = "1.0.0.0";
+            // 
+            // NewVersionLabel
+            // 
+            this.NewVersionLabel.AutoSize = true;
+            this.NewVersionLabel.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NewVersionLabel.Location = new System.Drawing.Point(220, 102);
+            this.NewVersionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.NewVersionLabel.Name = "NewVersionLabel";
+            this.NewVersionLabel.Size = new System.Drawing.Size(60, 19);
+            this.NewVersionLabel.TabIndex = 4;
+            this.NewVersionLabel.Text = "1.1.0.0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(64, 150);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 23);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Changes:";
+            // 
+            // ChangesTextBox
+            // 
+            this.ChangesTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.ChangesTextBox.Location = new System.Drawing.Point(55, 176);
+            this.ChangesTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ChangesTextBox.Multiline = true;
+            this.ChangesTextBox.Name = "ChangesTextBox";
+            this.ChangesTextBox.ReadOnly = true;
+            this.ChangesTextBox.Size = new System.Drawing.Size(505, 218);
+            this.ChangesTextBox.TabIndex = 6;
+            // 
+            // OkButton
+            // 
+            this.OkButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OkButton.Location = new System.Drawing.Point(289, 414);
+            this.OkButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.OkButton.Name = "OkButton";
+            this.OkButton.Size = new System.Drawing.Size(131, 28);
+            this.OkButton.TabIndex = 7;
+            this.OkButton.Text = "Update Now";
+            this.OkButton.UseVisualStyleBackColor = true;
+            this.OkButton.Click += new System.EventHandler(this.OkButton_Click);
+            // 
+            // _CancelButton
+            // 
+            this._CancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this._CancelButton.Location = new System.Drawing.Point(441, 414);
+            this._CancelButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this._CancelButton.Name = "_CancelButton";
+            this._CancelButton.Size = new System.Drawing.Size(100, 28);
+            this._CancelButton.TabIndex = 8;
+            this._CancelButton.Text = "Cancel";
+            this._CancelButton.UseVisualStyleBackColor = true;
+            this._CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // UpdateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this._CancelButton;
+            this.ClientSize = new System.Drawing.Size(605, 457);
+            this.Controls.Add(this._CancelButton);
+            this.Controls.Add(this.OkButton);
+            this.Controls.Add(this.ChangesTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.NewVersionLabel);
+            this.Controls.Add(this.CurrentVersionLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "UpdateForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Update Alloclave";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
--- Visualizer/plugins/transport/ProcessSpyTransport/ProcessForm.Designer.cs
+++ Visualizer/plugins/transport/ProcessSpyTransport/ProcessForm.Designer.cs
@@ -28,69 +28,78 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this._CancelButton = new System.Windows.Forms.Button();
-			this.OkButton = new System.Windows.Forms.Button();
-			this.ProcessComboBox = new System.Windows.Forms.ComboBox();
-			this.ChooseTransportTypeLabel = new System.Windows.Forms.Label();
-			this.SuspendLayout();
-			// 
-			// CancelButton
-			// 
-			this._CancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this._CancelButton.Location = new System.Drawing.Point(333, 84);
-			this._CancelButton.Name = "CancelButton";
-			this._CancelButton.Size = new System.Drawing.Size(75, 23);
-			this._CancelButton.TabIndex = 7;
-			this._CancelButton.Text = "Cancel";
-			this._CancelButton.UseVisualStyleBackColor = true;
-			// 
-			// OkButton
-			// 
-			this.OkButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.OkButton.Location = new System.Drawing.Point(242, 84);
-			this.OkButton.Name = "OkButton";
-			this.OkButton.Size = new System.Drawing.Size(75, 23);
-			this.OkButton.TabIndex = 6;
-			this.OkButton.Text = "OK";
-			this.OkButton.UseVisualStyleBackColor = true;
-			// 
-			// ProcessComboBox
-			// 
-			this.ProcessComboBox.DropDownHeight = 300;
-			this.ProcessComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-			this.ProcessComboBox.FormattingEnabled = true;
-			this.ProcessComboBox.IntegralHeight = false;
-			this.ProcessComboBox.Location = new System.Drawing.Point(172, 22);
-			this.ProcessComboBox.MaxDropDownItems = 16;
-			this.ProcessComboBox.Name = "ProcessComboBox";
-			this.ProcessComboBox.Size = new System.Drawing.Size(247, 21);
-			this.ProcessComboBox.Sorted = true;
-			this.ProcessComboBox.TabIndex = 5;
-			// 
-			// ChooseTransportTypeLabel
-			// 
-			this.ChooseTransportTypeLabel.AutoSize = true;
-			this.ChooseTransportTypeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.ChooseTransportTypeLabel.Location = new System.Drawing.Point(23, 22);
-			this.ChooseTransportTypeLabel.Name = "ChooseTransportTypeLabel";
-			this.ChooseTransportTypeLabel.Size = new System.Drawing.Size(129, 17);
-			this.ChooseTransportTypeLabel.TabIndex = 4;
-			this.ChooseTransportTypeLabel.Text = "Choose process:";
-			// 
-			// ProcessForm
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(458, 135);
-			this.Controls.Add(this._CancelButton);
-			this.Controls.Add(this.OkButton);
-			this.Controls.Add(this.ProcessComboBox);
-			this.Controls.Add(this.ChooseTransportTypeLabel);
-			this.Name = "ProcessForm";
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-			this.Text = "Choose Process";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this._CancelButton = new System.Windows.Forms.Button();
+            this.OkButton = new System.Windows.Forms.Button();
+            this.ProcessComboBox = new System.Windows.Forms.ComboBox();
+            this.ChooseTransportTypeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // _CancelButton
+            // 
+            this._CancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this._CancelButton.Location = new System.Drawing.Point(444, 103);
+            this._CancelButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this._CancelButton.Name = "_CancelButton";
+            this._CancelButton.Size = new System.Drawing.Size(100, 28);
+            this._CancelButton.TabIndex = 7;
+            this._CancelButton.Text = "Cancel";
+            this._CancelButton.UseVisualStyleBackColor = true;
+            // 
+            // OkButton
+            // 
+            this.OkButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.OkButton.Location = new System.Drawing.Point(323, 103);
+            this.OkButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.OkButton.Name = "OkButton";
+            this.OkButton.Size = new System.Drawing.Size(100, 28);
+            this.OkButton.TabIndex = 6;
+            this.OkButton.Text = "OK";
+            this.OkButton.UseVisualStyleBackColor = true;
+            // 
+            // ProcessComboBox
+            // 
+            this.ProcessComboBox.DropDownHeight = 300;
+            this.ProcessComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ProcessComboBox.FormattingEnabled = true;
+            this.ProcessComboBox.IntegralHeight = false;
+            this.ProcessComboBox.Location = new System.Drawing.Point(229, 27);
+            this.ProcessComboBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ProcessComboBox.MaxDropDownItems = 16;
+            this.ProcessComboBox.Name = "ProcessComboBox";
+            this.ProcessComboBox.Size = new System.Drawing.Size(328, 24);
+            this.ProcessComboBox.Sorted = true;
+            this.ProcessComboBox.TabIndex = 5;
+            // 
+            // ChooseTransportTypeLabel
+            // 
+            this.ChooseTransportTypeLabel.AutoSize = true;
+            this.ChooseTransportTypeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ChooseTransportTypeLabel.Location = new System.Drawing.Point(31, 27);
+            this.ChooseTransportTypeLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.ChooseTransportTypeLabel.Name = "ChooseTransportTypeLabel";
+            this.ChooseTransportTypeLabel.Size = new System.Drawing.Size(151, 20);
+            this.ChooseTransportTypeLabel.TabIndex = 4;
+            this.ChooseTransportTypeLabel.Text = "Choose process:";
+            // 
+            // ProcessForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 166);
+            this.ControlBox = false;
+            this.Controls.Add(this._CancelButton);
+            this.Controls.Add(this.OkButton);
+            this.Controls.Add(this.ProcessComboBox);
+            this.Controls.Add(this.ChooseTransportTypeLabel);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ProcessForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Choose Process";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
