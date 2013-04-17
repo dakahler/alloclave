0988bbfbb4bb727a549aed844f131e92ed574703
David Kahler <dakahler@gmail.com>
2013-04-16 20:07:49 -0700

    Changed version.cs to correct text.

--- Installer/version.cs
+++ Installer/version.cs
@@ -1,1 +1,15 @@
-0.1
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Drawing.Drawing2D;
+
+namespace Alloclave
+{
+	public static partial class Common
+	{
+		public const String Version = "0.1";
+	}
+}
